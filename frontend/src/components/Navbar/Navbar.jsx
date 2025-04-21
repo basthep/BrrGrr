61a4276a04fbf794e22cbf7db8e06405b44d0da6
basthep <abdulbasitherayampattil@gmail.com>
2025-04-21 11:57:10 +0530

    first commit

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div class="navbar">
+      <img src="" alt="" className='logo' />
+    </div>
+  )
+}
+
+export default Navbar
